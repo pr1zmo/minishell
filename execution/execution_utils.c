3301fa1d748e42863a3aff38b8573d4299b62257
prizmo <prizmo@gmail.com>
2024-08-14 11:16:13 +0100

    M

--- execution/execution_utils.c
+++ execution/execution_utils.c
@@ -66,11 +66,29 @@ static char	*get_full_cmd(char *av, char **env)
 	return (NULL);
 }
 
+<<<<<<< HEAD
 void	process_command(char *path, char **av, char **env)
 {
 	int	i;
 
 	i = 0;
+=======
+char	*special_char(char *cmd)
+{
+	if (ft_strncmp(cmd, '|', 1))
+}
+
+void	exec_command_list(char *path, char **cmd, char **av)
+{
+	int		i;
+	char	*special_cmd;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (special_cmd = special_char(cmd[i]))
+	}
+>>>>>>> 3d68ba5 (M)
 }
 
 int	exec_command(char **av, char **env)
@@ -87,12 +105,10 @@ int	exec_command(char **av, char **env)
 		perror(av[0]);
 		exit(1);
 	}
+<<<<<<< HEAD
 	process_command(path, av, env);
+=======
+	exec_command_list(path, cmd, av);
+>>>>>>> 3d68ba5 (M)
 	return (1);
-}
-
-void	check_arguments(char **av)
-{
-	if (av[2][0] == '\0' || av[3][0] == '\0')
-		error_exit("Bad arguments");
 }
