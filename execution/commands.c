939a1fe58e0ab48c0b295e893f7b3d4b4f5299fa
Zaid Elbassari <zelbassa@e2r11p11.1337.ma>
2024-10-25 18:54:07 +0100

    old ways

--- execution/commands.c
+++ execution/commands.c
@@ -6,7 +6,7 @@
 /*   By: zelbassa <zelbassa@1337.student.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 12:21:30 by zelbassa          #+#    #+#             */
-/*   Updated: 2024/10/24 23:40:21 by zelbassa         ###   ########.fr       */
+/*   Updated: 2024/10/25 18:53:44 by zelbassa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,9 +152,6 @@ void	complex_command(t_data *data)
 	if (data->cmd)
 	{
 		create_files(data->cmd, data);
-		// print_cmd_list(data);
-		// exit(0);
-		// show_command_info(data->cmd);
 		execute_cmds(data->cmd, data->envp_arr, data);
 	}
 	else
