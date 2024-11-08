d230e67e432046036b912058d810f499dbafbe13
primo <prizmo@mail.com>
2024-11-08 13:46:40 +0100

    OK, RELAX, STILL WORKING

--- execution/execution.c
+++ execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: zelbassa <zelbassa@1337.student.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 09:35:10 by prizmo            #+#    #+#             */
-/*   Updated: 2024/11/05 20:40:50 by zelbassa         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:46:09 by zelbassa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	handle_input(t_data *data)
 	int i = count_symbols(data);
 	if (i == 0)
 	{
-		cmd = array_to_string(temp);
+		// cmd = array_to_string(temp);
+		cmd = to_str(data->cmd->argv);
 		data->status = single_command(data, cmd);
 	}
 	else
@@ -29,6 +30,25 @@ int	handle_input(t_data *data)
 		complex_command(data);
 	}
 	return (0);
+}
+
+void	free_cmd(t_cmd **head)
+{
+	t_cmd	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = (*head);
+	while (*head)
+	{
+		i = 0;
+		while ((*head)->argv[i])
+			free((*head)->argv[i++]);
+		free((*head)->argv);
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
 }
 
 int	minishell(t_data *data)
@@ -57,6 +77,8 @@ int	minishell(t_data *data)
 		new_fd = open("temp.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
 		ft_putnbr_fd(data->status, new_fd);
 		g_exit_status = data->status;
+		free_cmd(&cmd);
+		free_line(&head);
 	}
 	return (data->status);
 }
